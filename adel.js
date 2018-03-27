76fa8947ef951bc26b34025a02a54621e7ca16d2
toChaim <toChaim@gmail.com>
2018-03-27 13:12:13 -0700

    change makeDictionary to promis. add makeAnagram.

--- adel.js
+++ adel.js
@@ -1,3 +1,37 @@
-const isWord = (word, dictionary) => !!dictionary[word];
+const isWord = (word, dictionary) => !!dictionary[word.toLowerCase()];
 
-module.exports = { isWord };
+const makeDictionary = (fileName) => {
+    const fs = require('fs');
+    var dict;
+    return new Promise((resolve, reject)=> {
+        fs.readFile(fileName, 'utf8', (err, data) => {
+            if(err){ reject(err); }
+            resolve (JSON.parse(data));
+        })
+         
+    });
+};
+
+const makeAnagram = (function(){
+    var memo = {};
+
+    return (word)=>{
+        word = word.split('');
+        var res = [];
+
+        const swap = (index)=>{
+            if(index === word.length){ res.push( word.join('') ); }
+            for(let i = index; i < word.length; i++){
+                if(index !== i && word[index] === word[i]){ continue; }
+                [ word[i], word[index] ]=[ word[index], word[i]];
+                swap(index+1);
+                [ word[i], word[index] ]=[ word[index], word[i]];
+            }
+        };
+    
+        memo[word] = memo[word] || swap(0);
+        return memo[word];
+    };
+})();
+
+module.exports = { isWord, makeDictionary };
