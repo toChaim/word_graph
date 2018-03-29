1ce5b3acd3eabf62a42110797ab16128dbf503eb
toChaim <toChaim@gmail.com>
2018-03-29 09:56:52 -0700

    replace old removeLetter with better removeLetter

--- spec/word_graph_test_spec.js
+++ spec/word_graph_test_spec.js
@@ -1,4 +1,4 @@
-const { isWord, makeDictionary, makeAnagramer, removeLetter, removeLetter2 } = require('../word_graph');
+const { isWord, makeDictionary, makeAnagramer, removeLetter } = require('../word_graph');
 const dick = {'word': []};
 
 describe("isWord", () => {
@@ -153,38 +153,6 @@ describe('removeLetter',()=>{
       let input = "cat";
       let output = ["at","ca","ct"].sort();
       let actual = removeLetter(input).sort();
-      expect(actual).toEqual(output);
-    });
-  });
-});
-
-describe('removeLetter2',()=>{
-  it('should be a function', ()=>{
-    expect(typeof removeLetter2).toBe('function');
-  });
-  describe('should return correct values', ()=>{
-    it('should return [] for "a"', ()=>{
-      let input = "a";
-      let output = [];
-      let actual = removeLetter2(input);
-      expect(actual).toEqual(output);
-    });
-    it('should return ["a"] for "aa"', ()=>{
-      let input = "aa";
-      let output = ["a"];
-      let actual = removeLetter2(input);
-      expect(actual).toEqual(output);
-    });
-    it('should return ["a","b"] for "ab"', ()=>{
-      let input = "ab";
-      let output = ["a","b"].sort();
-      let actual = removeLetter2(input).sort();
-      expect(actual).toEqual(output);
-    });
-    it('should return ["at","ca","ct"] for "cat"', ()=>{
-      let input = "cat";
-      let output = ["at","ca","ct"].sort();
-      let actual = removeLetter2(input).sort();
       expect(actual).toEqual(output);
     });
   });
