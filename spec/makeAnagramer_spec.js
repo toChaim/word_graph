cef39225bcaebed97212b4396a260273483ca78f
toChaim <toChaim@gmail.com>
2018-03-29 10:07:24 -0700

    split up testing files.

--- spec/makeAnagramer_spec.js
+++ spec/makeAnagramer_spec.js
@@ -1,54 +1,4 @@
-const { isWord, makeDictionary, makeAnagramer, removeLetter } = require('../word_graph');
-const dick = {'word': []};
-
-describe("isWord", () => {
-  describe('import', ()=>{
-    it("is a function", function() {
-      expect(typeof isWord).toBe('function');
-    });
-  });
-
-  describe('returns correct output', ()=>{
-    it('returns true given word', ()=>{
-      expect(isWord('word', dick)).toBe(true);
-    });
-    it('returns true given Word', ()=>{
-      expect(isWord('Word', dick)).toBe(true);
-    });
-    it('returns false given zword', ()=>{
-      expect(isWord('zword', dick)).toBe(false);
-    });
-  });
-});
-
-describe("makeDictionary", () => {
-  describe('import', ()=>{
-    it("is a function", function() {
-      expect(typeof makeDictionary).toBe('function');
-    });
-  });
-  describe('returns correct output', ()=>{
-    it('returns dict given a good file', (done)=>{
-      const dict = makeDictionary('./words_dictionary.json');
-      expect(dict instanceof Promise).toBe(true);
-      dict.then(data => { 
-        expect(Object.keys(data).length).toBe(370101);
-        done();
-      })
-      .catch(err => {
-        done(err);
-      });
-    });
-    it('throws error give bad file name', (done)=>{
-        makeDictionary('./words_dictionary.jsonn') 
-        .catch((err)=>{
-          expect(err.message).toBe("ENOENT: no such file or directory, open './words_dictionary.jsonn'");
-          expect(err.code).toBe('ENOENT');
-          done();
-        });
-    });
-  });
-});
+const { makeAnagramer } = require('../word_graph');
 
 describe('makeAnagramer', ()=>{
   it('should be a function', ()=>{
@@ -122,38 +72,6 @@ describe('makeAnagramer', ()=>{
       expect(anagramer('gfedcba').length).toBe(5040);
       let testRun2Time = (Date.now() - timeBefore)*2;
       expect(testRun2Time).toBeLessThan(testRun1Time);
-    });
-  });
-});
-
-describe('removeLetter',()=>{
-  it('should be a function', ()=>{
-    expect(typeof removeLetter).toBe('function');
-  });
-  describe('should return correct values', ()=>{
-    it('should return [] for "a"', ()=>{
-      let input = "a";
-      let output = [];
-      let actual = removeLetter(input);
-      expect(actual).toEqual(output);
-    });
-    it('should return ["a"] for "aa"', ()=>{
-      let input = "aa";
-      let output = ["a"];
-      let actual = removeLetter(input);
-      expect(actual).toEqual(output);
-    });
-    it('should return ["a","b"] for "ab"', ()=>{
-      let input = "ab";
-      let output = ["a","b"].sort();
-      let actual = removeLetter(input).sort();
-      expect(actual).toEqual(output);
-    });
-    it('should return ["at","ca","ct"] for "cat"', ()=>{
-      let input = "cat";
-      let output = ["at","ca","ct"].sort();
-      let actual = removeLetter(input).sort();
-      expect(actual).toEqual(output);
     });
   });
 });
