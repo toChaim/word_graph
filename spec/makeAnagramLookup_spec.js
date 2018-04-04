a09a8771544cd2671083ab2b1e7146b92d30a33b
toChaim <toChaim@gmail.com>
2018-04-03 20:15:37 -0700

    change test to match new lookup

--- spec/makeAnagramLookup_spec.js
+++ spec/makeAnagramLookup_spec.js
@@ -1,97 +1,30 @@
 const { makeAnagramLookup } = require('../word_graph');
+const dic = {a:1, i:1, at:1, cat:1, act:1, tac:1};
 
 describe('makeAnagramLookup', ()=>{
   it('should be a function', ()=>{
     expect(typeof makeAnagramLookup).toBe('function');
   });
-  it('should return a function', ()=>{
-    let anagramer = makeAnagramLookup();
-    expect(typeof anagramer).toBe('function');
+  it('should return an object', ()=>{
+    let lookup = makeAnagramLookup(dic);
+    expect(typeof lookup).toBe('object');
   });
-  describe('anagramer should return correct results', ()=>{
-    it('for "a"', (done)=>{
-        makeAnagramLookup()
-        .then(lookup => {
-            let res = lookup('a');
-            expect(Array.isArray(res)).toBe(true);
-            expect(res.length).toBe(1);
-            done();
-        })
-        .catch(err => {
-            done(err);
-        });
-    });
-    it('for "aa"', (done)=>{
-        makeAnagramLookup()
-        .then(lookup => {
-            let res = lookup('aa');
-            expect(Array.isArray(res)).toBe(true);
-            expect(res.length).toBe(0);
-            done();
-        })
-        .catch(err => {
-            done(err);
-        });
-    });
-    it('for "Aa"', (done)=>{
-        makeAnagramLookup()
-        .then(lookup => {
-            let res = lookup('Aa');
-            expect(Array.isArray(res)).toBe(true);
-            expect(res.length).toBe(0);
-            done();
-        })
-        .catch(err => {
-            done(err);
-        });
-    });
-    it('for "Cat"', (done)=>{
-        makeAnagramLookup()
-        .then(lookup => {
-            let res = lookup('Cat');
-            expect(Array.isArray(res)).toBe(true);
-            expect(res.length).toBe(3);
-            done();
-        })
-        .catch(err => {
-            done(err);
-        });
-    });
-    it('for "adnor"', (done)=>{
-        makeAnagramLookup()
-        .then(lookup => {
-            let res = lookup('adnor');
-            expect(Array.isArray(res)).toBe(true);
-            expect(res.length).toBe(10);
-            done();
-        })
-        .catch(err => {
-            done(err);
-        });
-    });
+  describe('should have return correct value', ()=>{
+      it('for "a"',()=>{
+        let lookup = makeAnagramLookup(dic);
+        expect(JSON.stringify(lookup['a'].sort())).toBe(JSON.stringify(['a']));
+      });
+      it('for "act"',()=>{
+        let lookup = makeAnagramLookup(dic);
+        expect(JSON.stringify(lookup['act'].sort())).toBe(JSON.stringify(['act','cat','tac']));
+      });
+      it('for "at"',()=>{
+        let lookup = makeAnagramLookup(dic);
+        expect(JSON.stringify(lookup['at'].sort())).toBe(JSON.stringify(['at']));
+      });
+      it('for "aA"',()=>{
+        let lookup = makeAnagramLookup(dic);
+        expect(lookup['aA']).toBe(undefined);
+      });
   });
 });
-
-//   describe('memoization should make anagramer faster', ()=>{
-//     it('more then two times faster secnond time it is called', ()=>{
-//       let anagramer = makeAnagramLookup();
-//       let timeBefore = Date.now();
-//       expect(anagramer('abcdefgh').length).toBe(40320);
-//       let testRun1Time = Date.now() - timeBefore;
-//       timeBefore = Date.now();
-//       expect(anagramer('abcdefgh').length).toBe(40320);
-//       let testRun2Time = (Date.now() - timeBefore)*2;
-//       expect(testRun2Time).toBeLessThan(testRun1Time);
-//     });
-//     it('more then two times faster secnond time it is called on anagram of first call', ()=>{
-//       let anagramer = makeAnagramLookup();
-//       let timeBefore = Date.now();
-//       expect(anagramer('abcdefgh').length).toBe(40320);
-//       let testRun1Time = Date.now() - timeBefore;
-//       timeBefore = Date.now();
-//       expect(anagramer('gfedcbah').length).toBe(40320);
-//       let testRun2Time = (Date.now() - timeBefore)*2;
-//       expect(testRun2Time).toBeLessThan(testRun1Time);
-//     });
-//   });
-// });
